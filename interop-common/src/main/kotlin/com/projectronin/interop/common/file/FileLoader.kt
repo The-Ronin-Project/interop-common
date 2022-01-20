25e4c7ed7820132ef2f5c452b9a38d13017cfdd6
Josh Smith <43484632+arssycro@users.noreply.github.com>
2022-01-20 11:40:48 -0600

    INT-296: Add more comprehensive support for loading files (#7)

--- interop-common/src/main/kotlin/com/projectronin/interop/common/file/FileLoader.kt
+++ interop-common/src/main/kotlin/com/projectronin/interop/common/file/FileLoader.kt
@@ -10,7 +10,8 @@ import java.io.FileReader
  */
 class FileLoader private constructor() {
     companion object {
-        private val CLASSPATH_PREFIX = "classpath:"
+        private const val CLASSPATH_PREFIX = "classpath:"
+        private const val FILE_PREFIX = "file:"
 
         /**
          * Loads the supplied [fileName] from the classpath or underlying system and returns the contents of the file.
@@ -20,14 +21,30 @@ class FileLoader private constructor() {
         fun loadFile(fileName: String): String {
             if (fileName.startsWith(CLASSPATH_PREFIX)) {
                 val classpathFilename = fileName.substring(CLASSPATH_PREFIX.length)
-                val stream = this::class.java.classLoader.getResourceAsStream(classpathFilename)
+                return loadClasspathFile(classpathFilename)
                     ?: throw FileNotFoundException("$classpathFilename not found on classpath")
-                return String(stream.readAllBytes())
+            }
+            if (fileName.startsWith(FILE_PREFIX)) {
+                val fileFilename = fileName.substring(FILE_PREFIX.length)
+                return loadSystemFile(fileFilename) ?: throw FileNotFoundException("$fileFilename not found on system")
             }
 
+            // Since it wasn't prefixed, we'll attempt to find it on the Classpath first, and then fall back to the System.
+            val contents = loadClasspathFile(fileName)
+            contents?.let { return it }
+
+            return loadSystemFile(fileName) ?: throw FileNotFoundException("$fileName does not exist")
+        }
+
+        private fun loadClasspathFile(fileName: String): String? {
+            val stream = this::class.java.classLoader.getResourceAsStream(fileName) ?: return null
+            return String(stream.readAllBytes())
+        }
+
+        private fun loadSystemFile(fileName: String): String? {
             val file = File(fileName)
             if (!file.exists()) {
-                throw FileNotFoundException("$fileName does not exist")
+                return null
             }
             return FileReader(file).use { it.readText() }
         }
--- interop-common/src/test/kotlin/com/projectronin/interop/common/file/FileLoaderTest.kt
+++ interop-common/src/test/kotlin/com/projectronin/interop/common/file/FileLoaderTest.kt
@@ -7,34 +7,54 @@ import java.io.FileNotFoundException
 
 class FileLoaderTest {
     @Test
-    fun `throws exception when classpath file is not found`() {
+    fun `throws exception when classpath-prefixed file is not found`() {
         val exception = assertThrows<FileNotFoundException> { FileLoader.loadFile("classpath:unknown.txt") }
         assertEquals("unknown.txt not found on classpath", exception.message)
     }
 
     @Test
-    fun `loads top level classpath file`() {
+    fun `loads top level classpath-prefixed file`() {
         val contents = FileLoader.loadFile("classpath:Sample1.txt")
         assertEquals("This is the first sample.\n", contents)
     }
 
     @Test
-    fun `loads nested classpath file`() {
+    fun `loads nested classpath-prefixed file`() {
         val contents = FileLoader.loadFile("classpath:nested/Sample2.txt")
         assertEquals("This is the second, nested sample.\n", contents)
     }
 
     @Test
-    fun `throws exception when file system file is not found`() {
-        val exception = assertThrows<FileNotFoundException> { FileLoader.loadFile("/User/Unknown/unknown.txt") }
-        assertEquals("/User/Unknown/unknown.txt does not exist", exception.message)
+    fun `throws exception when file-prefixed file is not found`() {
+        val exception = assertThrows<FileNotFoundException> { FileLoader.loadFile("file:/User/Unknown/unknown.txt") }
+        assertEquals("/User/Unknown/unknown.txt not found on system", exception.message)
     }
 
     @Test
-    fun `loads nested file system file`() {
+    fun `loads file-prefixed file`() {
+        // First we have to get the absolute path to the resource to ensure this works for every system
+        val url = this.javaClass.classLoader.getResource("Sample1.txt")
+        val contents = FileLoader.loadFile("file:${url.path}")
+        assertEquals("This is the first sample.\n", contents)
+    }
+
+    @Test
+    fun `loads file from classpath`() {
+        val contents = FileLoader.loadFile("Sample1.txt")
+        assertEquals("This is the first sample.\n", contents)
+    }
+
+    @Test
+    fun `loads file from system`() {
         // First we have to get the absolute path to the resource to ensure this works for every system
         val url = this.javaClass.classLoader.getResource("Sample1.txt")
         val contents = FileLoader.loadFile(url.path)
         assertEquals("This is the first sample.\n", contents)
+    }
+
+    @Test
+    fun `throws exception when file not found on classpath or system`() {
+        val exception = assertThrows<FileNotFoundException> { FileLoader.loadFile("/unknown.txt") }
+        assertEquals("/unknown.txt does not exist", exception.message)
     }
 }
