de60cc5e6afd0b3037fca1c06eba81e8db3eb830
Sam Felt <samuel.edward.felt@gmail.com>
2022-09-23 09:43:40 -0700

    [INT-1014] move utils to common (#73)

---
+++ interop-common-jackson/src/main/kotlin/com/projectronin/interop/common/jackson/JacksonUtil.kt
@@ -0,0 +1,29 @@
+package com.projectronin.interop.common.jackson
+
+import kotlin.reflect.KClass
+
+object JacksonUtil {
+    private val mapper = JacksonManager.objectMapper
+
+    /**
+     * Deserializes a json array into a List<> of [clazz].
+     */
+    fun <T : Any> readJsonList(json: String, clazz: KClass<T>): List<T> {
+        val type = mapper.typeFactory.constructCollectionType(List::class.java, clazz.java)
+        return mapper.readValue(json, type)
+    }
+
+    /**
+     * Deserializes a single json object of type [clazz]
+     */
+    fun <T : Any> readJsonObject(json: String, clazz: KClass<T>): T {
+        return mapper.readValue(json, clazz.java)
+    }
+
+    /**
+     * Wrapper around JacksonManager.objectMapper.writeValueAsString
+     */
+    fun writeJsonValue(value: Any): String {
+        return mapper.writeValueAsString(value)
+    }
+}
---
+++ interop-common-jackson/src/test/kotlin/com/projectronin/interop/common/jackson/JacksonUtilTest.kt
@@ -0,0 +1,39 @@
+package com.projectronin.interop.common.jackson
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class JacksonUtilTest {
+
+    @Test
+    fun `multiple types work`() {
+        val example = ExampleObject("first")
+        val example2 = ExampleSecondObject("second")
+        val exampleJson = JacksonUtil.writeJsonValue(example)
+        val example2Json = JacksonUtil.writeJsonValue(example2)
+        val newExample = JacksonUtil.readJsonObject(exampleJson, ExampleObject::class)
+        val newExample2 = JacksonUtil.readJsonObject(example2Json, ExampleSecondObject::class)
+        assertEquals(example, newExample)
+        assertEquals(example2, newExample2)
+    }
+
+    @Test
+    fun `multiple lists work`() {
+        val example = listOf(ExampleObject("first"))
+        val example2 = listOf(ExampleSecondObject("second"))
+        val exampleJson = JacksonUtil.writeJsonValue(example)
+        val example2Json = JacksonUtil.writeJsonValue(example2)
+        val newExample = JacksonUtil.readJsonList(exampleJson, ExampleObject::class)
+        val newExample2 = JacksonUtil.readJsonList(example2Json, ExampleSecondObject::class)
+        assertEquals(example, newExample)
+        assertEquals(example2, newExample2)
+    }
+}
+
+private data class ExampleObject(
+    val first: String,
+)
+
+private data class ExampleSecondObject(
+    val second: String
+)
