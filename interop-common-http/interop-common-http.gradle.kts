e5d66a374de8990ec905811c079aca116b841d6a
Josh Smith <43484632+arssycro@users.noreply.github.com>
2022-10-04 10:11:05 -0500

    INT-1105: Switch to OkHttp engine for improved tracibility (#79)

--- interop-common-http/interop-common-http.gradle.kts
+++ interop-common-http/interop-common-http.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation("org.springframework:spring-context")
 
     implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.client.logging)
     implementation(libs.ktor.serialization.jackson)
--- interop-common-http/src/main/kotlin/com/projectronin/interop/common/http/spring/HttpSpringConfig.kt
+++ interop-common-http/src/main/kotlin/com/projectronin/interop/common/http/spring/HttpSpringConfig.kt
@@ -3,7 +3,7 @@ package com.projectronin.interop.common.http.spring
 import com.projectronin.interop.common.http.ktor.ContentLengthSupplier
 import com.projectronin.interop.common.jackson.JacksonManager
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
@@ -20,7 +20,7 @@ class HttpSpringConfig {
         // Now that we're handling exceptions directly, we no longer want to set the expectSuccess flag.
         // Doing so causes Ktor to throw a general exception before we can read the Http response status and
         // decide what to do with it.  See https://ktor.io/docs/response-validation.html
-        return HttpClient(CIO) {
+        return HttpClient(OkHttp) {
             install(ContentNegotiation) {
                 jackson {
                     JacksonManager.setUpMapper(this)
