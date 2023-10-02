e256069d38b50f33010fe0ef32404e7488d01ed4
Sam Felt <sfelt@projectronin.com>
2023-10-02 14:58:11 -0700

    [INT-2144] Make BaseInteropDAO spring compatible (#185)
    * [INT-2144] Make BaseInteropDAO spring compatible
    
    * use correct number of asterisks

--- interop-common-ktorm/src/main/kotlin/com/projectronin/interop/common/ktorm/dao/BaseInteropDAO.kt
+++ interop-common-ktorm/src/main/kotlin/com/projectronin/interop/common/ktorm/dao/BaseInteropDAO.kt
@@ -6,16 +6,16 @@ import org.ktorm.database.Database
 import org.ktorm.entity.Entity
 import org.ktorm.schema.Column
 
-/***
+/**
  * Base class we can use which implements a simple get for a primary key column
  */
-
 abstract class BaseInteropDAO<T : Entity<T>, K : Any>(val database: Database) {
     val logger = KotlinLogging.logger { }
 
     abstract val primaryKeyColumn: Column<K>
 
-    fun getByID(primaryKeyLookup: K): T? {
+    // This has to be open or else if you use this in a Spring context it'll start failing
+    open fun getByID(primaryKeyLookup: K): T? {
         return database.singleValueLookup<K, T>(primaryKeyLookup, primaryKeyColumn)
     }
 }
